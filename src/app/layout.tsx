0f18b2467bf00d7aa4349d0a6dae977828ddc788
CincoD2 <umsadiza@yahoo.es>
2026-02-04 08:52:23 +0100

    fix deploy issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,12 @@ export const metadata: Metadata = {
       { url: '/favicon.png', type: 'image/png' },
       { url: '/logourg.png', type: 'image/png' },
     ],
-    apple: [\n      { url: '/apple-touch-icon-180x180.png', sizes: '180x180' },\n      { url: '/apple-touch-icon-167x167.png', sizes: '167x167' },\n      { url: '/apple-touch-icon-152x152.png', sizes: '152x152' },\n      { url: '/apple-touch-icon-120x120.png', sizes: '120x120' },\n    ],
+    apple: [
+      { url: '/apple-touch-icon-180x180.png', sizes: '180x180' },
+      { url: '/apple-touch-icon-167x167.png', sizes: '167x167' },
+      { url: '/apple-touch-icon-152x152.png', sizes: '152x152' },
+      { url: '/apple-touch-icon-120x120.png', sizes: '120x120' },
+    ],
     shortcut: '/favicon.png',
   },
   openGraph: {
