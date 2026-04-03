d61ee538c82f43d491fb8d232c63391126c5c1fd
Santiago <umsadiza@yahoo.es>
2026-04-03 14:52:48 +0200

    feat(protocolos): actualiza sepsis y fiebre neutropénica con nuevo contenido visual y changelog del 2026-04-03

--- src/components/mdx/Pearl.tsx
+++ src/components/mdx/Pearl.tsx
@@ -2,7 +2,9 @@ export function Pearl({ children }: { children: React.ReactNode }) {
   return (
     <div className="border-l-4 border-blue-500 bg-blue-50 p-4 my-4 rounded-lg">
       <strong className="block text-blue-700">💡 Perla clínica</strong>
-      <div>{children}</div>
+      <div className="[&>p]:m-0 [&>p]:inline [&>p]:contents [&_a]:inline [&_a]:!inline">
+        {children}
+      </div>
     </div>
   );
 }
